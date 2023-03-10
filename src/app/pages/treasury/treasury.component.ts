13be01c4afe374392f627ec32e54e324a5232b64
Ghadi <yobiz668@gmail.com>
2023-03-10 20:34:43 +0200

    header and columns

--- src/app/pages/treasury/treasury.component.ts
+++ src/app/pages/treasury/treasury.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./treasury.component.scss']
 })
 export class TreasuryComponent {
-
+title : string = "Bricklayer Treasury";
+text1 : string = "Lorem ipsum dolor sit amet, consectetur";
+text2 : string = "adipiscing elit, sed do eisumod tepmpor";
+text3 : string = "incididunt ut labore et dolore magna aliqua";
 }
