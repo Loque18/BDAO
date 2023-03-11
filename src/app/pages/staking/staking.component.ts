b24f4bc10d434a18fde5ba42af68995dc1ecffc5
loque18 <madridx2010@gmail.com>
2023-03-11 11:23:44 -0500

    feat: staking

--- src/app/pages/staking/staking.component.ts
+++ src/app/pages/staking/staking.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-staking',
-  templateUrl: './staking.component.html',
-  styleUrls: ['./staking.component.scss']
+	selector: 'app-staking',
+	templateUrl: './staking.component.html',
+	styleUrls: ['./staking.component.scss'],
 })
-export class StakingComponent {
-
-}
+export class StakingComponent {}
--- src/app/pages/treasury/treasury.component.ts
+++ src/app/pages/treasury/treasury.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-treasury',
-  templateUrl: './treasury.component.html',
-  styleUrls: ['./treasury.component.scss']
+	selector: 'app-treasury',
+	templateUrl: './treasury.component.html',
+	styleUrls: ['./treasury.component.scss'],
 })
-export class TreasuryComponent {
-
-}
+export class TreasuryComponent {}
