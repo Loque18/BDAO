e2d330ab1621402b06e2d6927e122379499273cf
loque18 <madridx2010@gmail.com>
2023-03-13 11:43:31 -0500

    fix: fixed layout on staking page

--- src/app/pages/staking/staking.component.ts
+++ src/app/pages/staking/staking.component.ts
@@ -1,8 +1,36 @@
 import { Component } from '@angular/core';
 
+type Tx = {
+	amount: number;
+	date: Date;
+	totalDividends: number;
+	link: string;
+};
+
+function genTxs() {
+	const txs: Tx[] = [];
+
+	for (let i = 0; i < 10; i++) {
+		const tx: Tx = {
+			amount: Math.random() * 100,
+			date: new Date(),
+			totalDividends: Math.random() * 100,
+			link: 'https://example.com',
+		};
+
+		txs.push(tx);
+	}
+
+	return txs;
+}
+
 @Component({
 	selector: 'app-staking',
-	templateUrl: './staking.component.html',
+	templateUrl: './test.html',
 	styleUrls: ['./staking.component.scss'],
 })
-export class StakingComponent {}
+export class StakingComponent {
+	txs = genTxs();
+
+	constructor() {}
+}
