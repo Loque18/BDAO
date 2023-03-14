b5280745621423bb2784b43df76548297f66afdd
Ghadi <yobiz668@gmail.com>
2023-03-14 16:59:09 +0200

    started with vote proposal page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProposalsComponent } from './pages/proposals/proposals.component';
+import { VoteProposalComponent } from './pages/vote-proposal/vote-proposal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProposalsComponent,
+    VoteProposalComponent,
   ],
   imports: [
     BrowserModule,
