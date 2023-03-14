f30f26f6e0e65b885d6efe25c2dcdb47b2333a0a
loque18 <madridx2010@gmail.com>
2023-03-14 11:56:07 -0500

    feat: fixed font sizes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './pages/test/test.component';
 
 const routes: Routes = [
 	{
@@ -11,6 +12,11 @@ const routes: Routes = [
 		path: 'staking',
 		loadChildren: () =>
 			import('src/app/pages/staking/staking.module').then((m) => m.StakingModule),
+	},
+
+	{
+		path: 'test',
+		component: TestComponent,
 	},
 ];
 
