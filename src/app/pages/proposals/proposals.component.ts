362e6da255046ae90b25903a7c63d7d1ddc548df
Ghadi <yobiz668@gmail.com>
2023-03-17 16:03:56 +0200

    started on responsive design for proposals

--- src/app/pages/proposals/proposals.component.ts
+++ src/app/pages/proposals/proposals.component.ts
@@ -8,6 +8,9 @@ import { Iproposals , items } from 'src/app/constants/proposals';
 })
 export class ProposalsComponent {
 title = "Proposals"
+mobileText1 = "VIP-101 Risk Parameters"
+mobileText2 = "Adjustments for SXP, TRX"
+mobileText3 = "and ETH"
 
 items : Iproposals[] = items;
 }
